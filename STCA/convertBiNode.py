a36f6208911a7671441db089a72a1125800b2c79
Teeeeg <iitaoyupeng@icloud.com>
2022-07-01 22:40:46 +0800

    std input output

--- STCA/convertBiNode.py
+++ STCA/convertBiNode.py
@@ -1,5 +1,6 @@
 # Definition for a binary tree node.
 class TreeNode:
+
     def __init__(self, x):
         self.val = x
         self.left = None
@@ -7,13 +8,14 @@ class TreeNode:
 
 
 class Solution:
+
     def __init__(self) -> None:
         self.head = None
         self.pre = None
 
     def convertBiNode(self, root: TreeNode) -> TreeNode:
         self.convertBiNodeCore(root)
-        return self.head
+        return self.head  # type: ignore
 
     def convertBiNodeCore(self, root):
         if not root:
